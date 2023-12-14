d8f462c788100acc9f2d1ea842c6fd3678e0fef0
Plus1XP <evldev@protonmail.ch>
2023-12-14 17:30:15 +0000

    Recreate sections manually to fix textfield space fill

--- Pantry/View/ItemDetailsView.swift
+++ Pantry/View/ItemDetailsView.swift
@@ -8,105 +8,171 @@
 import SwiftUI
 
 struct ItemDetailsView: View {
-    @Environment(\.dismiss) var dismiss
-    @EnvironmentObject var itemStore: ItemStore
+    @Environment(\.colorScheme) private var colorScheme
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject private var itemStore: ItemStore
     @State var item: Item
     @Binding var canEditItem: Bool
+    
+    private let sectionTitleColor: Color = Color.secondary
+    
+    private let itemFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter
+    }()
 
     var body: some View {
-        Form {
-            Section(header: Text("Item Information").frame(maxWidth: .infinity, alignment: .center)) {
-                HStack {
-                    if self.canEditItem {
-                        EmojiTextField(text: Binding(get: {item.name ?? ""}, set: {item.name = $0}))
-                            .font(.largeTitle)
-                            .textFieldStyle(.plain)
-                            .multilineTextAlignment(.center)
-                            .disabled(!self.canEditItem)
-                            .frame(maxWidth: .infinity, alignment: .center)
-                    } else {
-                        TextField("Item Name", text: Binding(get: {item.name ?? ""}, set: {item.name = $0}))
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .font(.system(size: 50))
-                            .multilineTextAlignment(.center)
-                    }
+        VStack {
+            //FIXME: EmojionField icon size
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                if self.canEditItem {
+                    EmojiTextField(text: Binding(get: {item.name ?? ""}, set: {item.name = $0}))
+                        .fixedSize(horizontal: true, vertical: true)
+                } else {
+                    Text(item.name ?? "")
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 50))
+                        .multilineTextAlignment(.center)
                 }
-                .listRowBackground(Color.clear)
+            })
+            .padding(.bottom)
+            
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                Text("Quantity Remaining")
+                    .font(.footnote)
+                    .textCase(nil)
+                    .foregroundStyle(self.sectionTitleColor)
+            })
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                TextField("Quantity", text: Binding(get: {"\(item.quantity)"}, set: {item.quantity = Int64($0) ?? 0}))
+                    .multilineTextAlignment(.trailing)
+                    .disabled(!self.canEditItem)
+                Text("of")
+                TextField("Total", text: Binding(get: {"\(item.total)"}, set: {item.total = Int64($0) ?? 0}))
+                    .multilineTextAlignment(.leading)
+                    .disabled(!self.canEditItem)
+            }) 
+            .padding()
+            .frame(maxWidth: .infinity)
+            .foregroundColor(.primary)
+            .background(
+                RoundedRectangle(
+                    cornerRadius: 12,
+                    style: .continuous
+                )
+                .fill(setFieldBackgroundColor(colorScheme: self.colorScheme))
+            )
+            .padding(.leading)
+            .padding(.trailing)
+            .padding(.bottom)
+            
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                Text("Notes")
+                    .font(.footnote)
+                    .textCase(nil)
+                    .foregroundStyle(self.sectionTitleColor)
+            })
+            HStack(content: {
+                if self.canEditItem {
+                    TextEditor(text: Binding(get: {item.notes ?? ""}, set: {item.notes = $0}))
+                        .scrollContentBackground(.hidden) // <- Hide it
+                        .background(.clear) // To see this
+                } else {
+                    ScrollView { // <-- add scroll around Text
+                        Text(item.notes ?? "")
+                            .lineLimit(nil) // <-- tell Text to use as many lines as it needs (so no truncating)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading) // <-- tell Text to take the entire space available for ScrollView
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity) // <-- if needed, tell ScrollView to use the full size of its parent too
+                }
+            })
+            .padding()
+            .foregroundColor(.primary)
+            .background(
+                RoundedRectangle(
+                    cornerRadius: 12,
+                    style: .continuous
+                )
+                .fill(setFieldBackgroundColor(colorScheme: self.colorScheme))
+            )
+            .padding(.leading)
+            .padding(.trailing)
+            .padding(.bottom)
+            
+            if !self.canEditItem {
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                    Text("Last Modified")
+                        .font(.footnote)
+                        .textCase(nil)
+                        .foregroundStyle(self.sectionTitleColor)
+                })
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                    Text(item.modified!, formatter: self.itemFormatter)
+                })
+                .padding()
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.primary)
+                .background(
+                    RoundedRectangle(
+                        cornerRadius: 12,
+                        style: .continuous
+                    )
+                    .fill(setFieldBackgroundColor(colorScheme: self.colorScheme))
+                )
+                .padding(.leading)
+                .padding(.trailing)
+                .padding(.bottom)
             }
             
-            Section(header: Text("Quantity Remaining").frame(maxWidth: .infinity, alignment: .center)) {
-                HStack {
-                    TextField("Quantity", text: Binding(get: {"\(item.quantity)"}, set: {item.quantity = Int64($0) ?? 0}))
-                        .textFieldStyle(.plain)
-                        .multilineTextAlignment(.trailing)
-                        .disabled(!self.canEditItem)
-                    Text("of")
-                    TextField("Total", text: Binding(get: {"\(item.total)"}, set: {item.total = Int64($0) ?? 0}))
-                        .textFieldStyle(.plain)
-                        .multilineTextAlignment(.leading)
-                        .disabled(!self.canEditItem)
-                }
-            }
-            Section(header: Text("Notes").frame(maxWidth: .infinity, alignment: .center)) {
-                HStack {
-                    TextEditor(text: Binding(get: {item.notes ?? ""}, set: {item.notes = $0}))
-                        .frame(
-                            minWidth: 0,
-                            maxWidth: .infinity,
-                            minHeight: 0,
-                            maxHeight: .infinity,
-                            alignment: .center
-                        )
-                        .multilineTextAlignment(.center)
-                        .disabled(!self.canEditItem)
-                }
-            }
-            Section(header: Text("Last Modified").frame(maxWidth: .infinity, alignment: .center)) {
-                HStack {
-                    Text(item.modified!, formatter: itemFormatter)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
-            }
-            if canEditItem {
+            if self.canEditItem {
                 HStack{
                     Spacer()
                     Button("Save", systemImage: "plus.circle", action: {
-                        item.modified = Date()
-                        itemStore.saveChanges()
+                        self.item.modified = Date()
+                        self.itemStore.saveChanges()
                         self.canEditItem = false
-                        dismiss()
+                        self.dismiss()
                     })
                     .padding(.top)
                     .buttonStyle(.borderedProminent)
-                    .tint(.green)
+                    .tint(.green.opacity(0.9))
                     Spacer()
                     Button("Cancel", systemImage: "xmark.circle", action: {
-                        itemStore.discardChanges()
+                        self.itemStore.discardChanges()
                         self.canEditItem = false
-                        dismiss()
+                        self.dismiss()
                     })
                     .padding(.top)
                     .buttonStyle(.borderedProminent)
-                    .tint(.red)
+                    .tint(.red.opacity(0.9))
                     Spacer()
                 }
                 .foregroundStyle(.white, .white)
-                .listRowBackground(Color.clear)
                 .disabled(!self.canEditItem)
             }
-            
         }
+        .background(setViewBackgroundColor(colorScheme: self.colorScheme))
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .medium
-    formatter.timeStyle = .short
-    return formatter
-}()
+private func setViewBackgroundColor(colorScheme: ColorScheme) -> Color {
+    return colorScheme == .light ? Color.secondaryBackground : Color.background
+}
+
+private func setFieldBackgroundColor(colorScheme: ColorScheme) -> Color {
+    return colorScheme == .light ? Color.background : Color.secondaryBackground
+}
 
 #Preview {
     ItemDetailsView(item: PersistenceController.shared.sampleItem, canEditItem: .constant(false))
         .environmentObject(ItemStore())
+        .preferredColorScheme(.light)
+}
+
+#Preview {
+    ItemDetailsView(item: PersistenceController.shared.sampleItem, canEditItem: .constant(false))
+        .environmentObject(ItemStore())
+        .preferredColorScheme(.dark)
 }
--- Pantry/View/NoteDetailsView.swift
+++ Pantry/View/NoteDetailsView.swift
@@ -8,107 +8,160 @@
 import SwiftUI
 
 struct NoteDetailsView: View {
-    @Environment(\.dismiss) var dismiss
-    @EnvironmentObject var noteStore: NoteStore
+    @Environment(\.colorScheme) private var colorScheme
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject private var noteStore: NoteStore
     @State var note: Note
-    @State var isPinnedTrigger: Bool = false
+    @State private var isPinnedTrigger: Bool = false
     @Binding var canEditNote: Bool
     @FocusState private var isNoteFocused: Bool
     
-        
+    private let sectionTitleColor: Color = Color.secondary
+    
+    private let itemFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
     var body: some View {
-        Form {
-            Section {
-//                     TextEditor does not have a placeholder
-//                     Using a ZStack & FocusState as a work around.
-                    ZStack(alignment: .topLeading) {
-                        TextEditor(text: Binding(get: {note.body ?? ""}, set: {note.body = $0}))
-                            .focused($isNoteFocused)
-                            .multilineTextAlignment(.center)
-                            .disableAutocorrection(false)
-                        if !isNoteFocused && ((note.body?.isEmpty) == nil) {
-                            Text("Notes of new item")
-                                .multilineTextAlignment(.leading)
-                                .disableAutocorrection(false)
-                                .foregroundColor(Color(uiColor: .placeholderText))
-                                .padding(.top, 10)
-                                .allowsHitTesting(false)
-                        }
-                    }
+        VStack {
+            ZStack {
+                TextField("Untitled Note", text: Binding(get: {note.name ?? ""}, set: {note.name = $0}))
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+                    .textCase(nil)
+                    .disableAutocorrection(false)
                     .disabled(!self.canEditNote)
-            } header: {
-                ZStack {
-                    TextField("Name of new note", text: Binding(get: {note.name ?? ""}, set: {note.name = $0}))
-                        .font(.title2)
-                        .multilineTextAlignment(.center)
+                Toggle(isOn: $isPinnedTrigger) {
+                    Text(Image(systemName: self.isPinnedTrigger ? "pin.fill" : "pin"))
+                }
+                .foregroundStyle(.orange, .orange)
+                .toggleStyle(.button)
+                .tint(.clear)
+                .font(.title3)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .disabled(!self.canEditNote)
+                .onChange(of: self.isPinnedTrigger, {
+                    note.isPinned = self.isPinnedTrigger
+                })
+            }
+            .padding(.leading)
+            .padding(.trailing)
+            .padding(.bottom)
+            
+            // TextEditor does not have a placeholder Using a
+            // ZStack & FocusState as a work around.
+            ZStack(alignment: .topLeading) {
+                if self.canEditNote {
+                    TextEditor(text: Binding(get: {note.body ?? ""}, set: {note.body = $0}))
+                        .scrollContentBackground(.hidden) // <- Hide it
+                        .background(.clear) // To see this
+                        .focused($isNoteFocused)
+                } else {
+                    ScrollView { // <-- add scroll around Text
+                        Text(note.body ?? "")
+                            .lineLimit(nil) // <-- tell Text to use as many lines as it needs (so no truncating)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading) // <-- tell Text to take the entire space available for ScrollView
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity) // <-- if needed, tell ScrollView to use the full size of its parent too
+                }
+                if !self.isNoteFocused && (note.body == "") {
+                    Text("No additional text")
+                        .foregroundColor(Color(uiColor: .placeholderText))
+                        .multilineTextAlignment(.leading)
+                        .allowsHitTesting(false)
+                }
+            }
+            .padding()
+            .foregroundColor(.primary)
+            .background(
+                RoundedRectangle(
+                    cornerRadius: 12,
+                    style: .continuous
+                )
+                .fill(setFieldBackgroundColor(colorScheme: self.colorScheme))
+            )
+            .padding(.leading)
+            .padding(.trailing)
+            .padding(.bottom)
+
+            if !self.canEditNote {
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                    Text("Last Modified")
+                        .font(.footnote)
                         .textCase(nil)
-                        .disableAutocorrection(false)
-                        .disabled(!self.canEditNote)
-                    Toggle(isOn: $isPinnedTrigger) {
-                        Text(Image(systemName: isPinnedTrigger ? "pin.fill" : "pin"))
-                    }
-                    .foregroundStyle(.orange, .orange)
-                    .toggleStyle(.button)
-                    .tint(.clear)
-                    .font(.title3)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    .disabled(!self.canEditNote)
-                    .onChange(of: isPinnedTrigger, {
-                        note.isPinned = isPinnedTrigger
-                    })
-                }
-            } footer: {
-                
+                        .foregroundStyle(self.sectionTitleColor)
+                })
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                    Text(note.modified!, formatter: self.itemFormatter)
+                })
+                .padding()
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.primary)
+                .background(
+                    RoundedRectangle(
+                        cornerRadius: 12,
+                        style: .continuous
+                    )
+                    .fill(setFieldBackgroundColor(colorScheme: self.colorScheme))
+                )
+                .padding(.leading)
+                .padding(.trailing)
+                .padding(.bottom)
             }
-            Section(header: Text("Last Modified").frame(maxWidth: .infinity, alignment: .center)) {
-                HStack {
-                    Text(note.modified!, formatter: itemFormatter)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
-            }
-            if canEditNote {
+            
+            if self.canEditNote {
                 HStack{
                     Spacer()
                     Button("Save", systemImage: "plus.circle", action: {
-                        note.modified = Date()
-                        noteStore.saveChanges()
+                        self.note.modified = Date()
+                        self.noteStore.saveChanges()
                         self.canEditNote = false
-                        dismiss()
+                        self.dismiss()
                     })
                     .padding(.top)
                     .buttonStyle(.borderedProminent)
-                    .tint(.green)
+                    .tint(.green.opacity(0.9))
                     Spacer()
                     Button("Cancel", systemImage: "xmark.circle", action: {
-                        noteStore.discardChanges()
+                        self.noteStore.discardChanges()
                         self.canEditNote = false
-                        dismiss()
+                        self.dismiss()
                     })
                     .padding(.top)
                     .buttonStyle(.borderedProminent)
-                    .tint(.red)
+                    .tint(.red.opacity(0.9))
                     Spacer()
                 }
                 .foregroundStyle(.white, .white)
-                .listRowBackground(Color.clear)
                 .disabled(!self.canEditNote)
             }
-            
         }
+        .background(setViewBackgroundColor(colorScheme: self.colorScheme))
         .onAppear(perform: {
-            isPinnedTrigger = note.isPinned
+            self.isPinnedTrigger = note.isPinned
         })
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .medium
-    formatter.timeStyle = .short
-    return formatter
-}()
+private func setViewBackgroundColor(colorScheme: ColorScheme) -> Color {
+    return colorScheme == .light ? Color.secondaryBackground : Color.background
+}
+
+private func setFieldBackgroundColor(colorScheme: ColorScheme) -> Color {
+    return colorScheme == .light ? Color.background : Color.secondaryBackground
+}
 
 #Preview {
     NoteDetailsView(note: PersistenceController.shared.sampleNote, canEditNote: .constant(false))
         .environmentObject(NoteStore())
+        .preferredColorScheme(.light)
+}
+
+#Preview {
+    NoteDetailsView(note: PersistenceController.shared.sampleNote, canEditNote: .constant(false))
+        .environmentObject(NoteStore())
+        .preferredColorScheme(.dark)
 }
